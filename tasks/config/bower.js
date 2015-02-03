89471a8a3110f9314f50d9e9f0e6ad5056f76156
Ronald Suez <rsuez93@gmail.com>
2015-02-03 00:16:48 -0430

    first remote

---
+++ tasks/config/bower.js
@@ -0,0 +1,34 @@
+/**
+ * Install bower components.
+ *
+ * ---------------------------------------------------------------
+ *
+ * Installs bower components and copies the required files into the assets folder structure.
+ *
+ */
+
+module.exports = function(grunt) {
+
+	grunt.config.set('bower', {
+		install: {
+			options: {
+				targetDir: './assets/vendor',
+				layout: 'byType',
+				install: true,
+				verbose: false,
+				cleanTargetDir: true,
+				cleanBowerDir: true,
+				bowerOptions: {}
+			}
+		},
+    dev: {
+      dest: './assets/',
+      js_dest: './assets/js/vendor',
+      css_dest: './assets/styles',
+      cleanTargetDir: true,
+      cleanBowerDir: true,
+    }
+	});
+
+  grunt.loadNpmTasks('grunt-bower');
+};
